6c924c089d9d8d055b623281a99a3f5c0e71b1e3
Arnaud Kervern <akervern@nuxeo.com>
2015-12-01 11:43:09 +0100

    NXP-18510: Extract timeoff entry component

--- src/main/js/app/scripts/app.js
+++ src/main/js/app/scripts/app.js
@@ -15,6 +15,9 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
+  // Sets Momentjs local
+  moment.locale(navigator.language || navigator.browserLanguage);
+
   // Sets app default base URL
   app.baseUrl = '/';
   if (window.location.port === '') { // if production
